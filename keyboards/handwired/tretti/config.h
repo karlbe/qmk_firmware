ace0fa7a797fc1ff5c220898d2d208c3f67c2857
Karl <karl.bernstal@gmail.com>
2021-02-24 16:05:50 +0100

    Added new gherkin handwire called tretti

---
+++ keyboards/handwired/tretti/config.h
@@ -0,0 +1,44 @@
+/* Copyright 2021 Karl Bernstål <karl.bernstal@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include "config_common.h"
+
+#define VENDOR_ID 0xBEEF
+#define PRODUCT_ID 0x002B
+#define DEVICE_VER 0x0001
+#define MANUFACTURER karlboards
+#define PRODUCT tretti
+
+#define MATRIX_ROWS 3
+#define MATRIX_COLS 10
+
+//B0 B1 B3 B4 B5 B6 B7 B8 B9 B10 B11 B12 B13 B14 B15
+//A2 A3 A4 A5 A6 A7 A8 A9 A10
+#define MATRIX_ROW_PINS { A4, A3, A2 }
+#define MATRIX_COL_PINS { A5, A6, A7, B3, B4, B5, B6, B7, B8, B9 }
+
+#define UNUSED_PINS B11, B12, A9, A10, A6, A5
+
+#define DIODE_DIRECTION COL2ROW
+
+//#define BACKLIGHT_PIN           A0
+//#define BACKLIGHT_LEVELS         5
+
+//#define MOUSEKEY_INTERVAL 32
+
+//#define TAPPING_TERM 175
---
+++ keyboards/handwired/tretti/rules.mk
@@ -0,0 +1,30 @@
+# MCU name
+MCU = STM32F401
+
+# Address of the bootloader in system memory
+STM32_BOOTLOADER_ADDRESS = 0x1FFF0000
+
+# Build Options
+#   change yes to no to disable
+#
+BOOTMAGIC_ENABLE = full     # Virtual DIP switch configuration
+MOUSEKEY_ENABLE = no       # Mouse keys
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+KEYBOARD_SHARED_EP = yes
+CONSOLE_ENABLE = no         # Console for debug
+COMMAND_ENABLE = no         # Commands for debug and configuration
+# Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
+SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
+# if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
+NKRO_ENABLE = yes           # USB Nkey Rollover
+BACKLIGHT_ENABLE = no      # Enable keyboard backlight functionality
+#BACKLIGHT_DRIVER = software
+RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
+BLUETOOTH_ENABLE = no       # Enable Bluetooth
+AUDIO_ENABLE = no           # Audio output
+#AUTO_SHIFT_ENABLE = yes
+
+# fix for arm linking with auto shift: https://github.com/qmk/qmk_firmware/issues/8809
+EXTRAFLAGS += --specs=nosys.specs
+
+LAYOUTS = ortho_3x10
